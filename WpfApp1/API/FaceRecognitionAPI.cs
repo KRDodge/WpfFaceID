3fe4395042e7b1bdf0e2d03d6f9bb55e3b75aaab
dodge <20012011ryu@gmail.com>
2021-12-14 10:07:45 +0900

    face ID 프로젝트 시작
    일단 naver clova api 기반으로 생성

---
+++ WpfApp1/API/FaceRecognitionAPI.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.IO;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using Newtonsoft.Json.Linq;
+
+namespace WpfFaceID.API
+{
+    public class FaceRecognitionAPI
+    {
+        public List<List<int>> GetFaceRectangleList(string _filePath)
+        {
+            List<List<int>> faceRectangle = new List<List<int>>();
+            JToken json = GetFaceJson(_filePath);
+            if (json == null)
+                return faceRectangle;
+
+            JToken BodyJToken = json.SelectToken("faces");
+            if (BodyJToken == null)
+                return faceRectangle;
+
+
+            int i = 0;
+            foreach (JToken bodyMembers in BodyJToken)
+            {
+                faceRectangle.Add(new List<int>());
+                JToken roiToken = bodyMembers.SelectToken("roi");
+
+                faceRectangle[i].Add((int)roiToken["x"]);
+                faceRectangle[i].Add((int)roiToken["y"]);
+                faceRectangle[i].Add((int)roiToken["width"]);
+                faceRectangle[i].Add((int)roiToken["height"]);
+                faceRectangle[i].Add((int)roiToken["height"]);
+
+                JToken genderToken = bodyMembers.SelectToken("gender");
+                string gender = genderToken["value"].ToString();
+                string gconfidence = genderToken["confidence"].ToString();
+
+                JToken ageToken = bodyMembers.SelectToken("age");
+                string aeg = ageToken["value"].ToString();
+                string aconfidence = ageToken["confidence"].ToString();
+                //string landmark = members["roi"].ToString();
+                //Console.WriteLine(members["roi"]);
+                //Console.WriteLine(members["landmark"]);
+                ++i;
+            }
+
+            return faceRectangle;
+        }
+
+        public JObject GetFaceJson(string _filePath)
+        {
+            JObject json = new JObject();
+
+            string boundary = "----------------------------" + DateTime.Now.Ticks.ToString("x");
+            string FilePath = _filePath;
+            FileStream fs = new FileStream(FilePath, FileMode.Open, FileAccess.Read);
+            byte[] fileData = new byte[fs.Length];
+            fs.Read(fileData, 0, fileData.Length);
+            fs.Close();
+
+            string CRLF = "\r\n";
+            string postData = "--" + boundary + CRLF + "Content-Disposition: form-data; name=\"image\"; filename=\"";
+            postData += Path.GetFileName(FilePath) + "\"" + CRLF + "Content-Type: image/jpeg" + CRLF + CRLF;
+            string footer = CRLF + "--" + boundary + "--" + CRLF;
+
+            Stream DataStream = new MemoryStream();
+            DataStream.Write(Encoding.UTF8.GetBytes(postData), 0, Encoding.UTF8.GetByteCount(postData));
+            DataStream.Write(fileData, 0, fileData.Length);
+            DataStream.Write(Encoding.UTF8.GetBytes("\r\n"), 0, 2);
+            DataStream.Write(Encoding.UTF8.GetBytes(footer), 0, Encoding.UTF8.GetByteCount(footer));
+            DataStream.Position = 0;
+            byte[] formData = new byte[DataStream.Length];
+            DataStream.Read(formData, 0, formData.Length);
+            DataStream.Close();
+
+            //string url = "https://openapi.naver.com/v1/vision/celebrity"; // 유명인 얼굴 인식
+            string url = "https://openapi.naver.com/v1/vision/face"; // 얼굴 감지
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Headers.Add("X-Naver-Client-Id", "pgfawO38TvY3Pqfm8HAO");
+            request.Headers.Add("X-Naver-Client-Secret", "IHaTD_mTtf");
+            request.Method = "POST";
+            request.ContentType = "multipart/form-data; boundary=" + boundary;
+            request.ContentLength = formData.Length;
+
+            using (Stream requestStream = request.GetRequestStream())
+            {
+                requestStream.Write(formData, 0, formData.Length);
+                requestStream.Close();
+            }
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream stream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+                string result;
+                result = reader.ReadToEnd();
+                stream.Close();
+                response.Close();
+                reader.Close();
+
+                json = JObject.Parse(result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return json;
+        }
+
+        
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using WpfFaceID.API;
+
+namespace WpfFaceID
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void ImageDrop(object sender, DragEventArgs e)
+        {
+            if(e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                if (files.Length > 1)
+                    Console.WriteLine("only one file availible");
+
+                string file = files[0];
+                setFaceImage(file);
+                drawFaceRectangle(file);
+            }
+        }
+
+        public void GetImageByFileDialog(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files(*.BMP; *.JPG; *.GIF)| *.BMP; *.JPG; *.GIF | All files(*.*) | *.*";
+            if (openFileDialog.ShowDialog() == true)
+                FilePathTextBox.Text = openFileDialog.FileName;
+
+            setFaceImage(openFileDialog.FileName);
+            drawFaceRectangle(openFileDialog.FileName);
+        }
+
+        public void GetImageByFilePath(object sender, RoutedEventArgs e)
+        {
+            if(e.Source != null)
+            {
+                string file = FilePathTextBox.Text;
+
+                setFaceImage(file);
+                drawFaceRectangle(file);
+            }
+        }
+
+        private void setFaceImage(string _filePath)
+        {
+            bool fileExist = File.Exists(_filePath);
+            string extension = System.IO.Path.GetExtension(_filePath);
+            bool checkExtension = checkImageExtension(extension);
+
+            if (fileExist == false && checkExtension == false)
+                return;
+
+            BitmapImage bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.UriSource = new Uri(_filePath);
+            bitmap.EndInit();
+
+            Image image = new Image();
+            image.Stretch = Stretch.None;
+            image.Source = bitmap;
+
+            image = resizeImage(image);
+
+            FaceStackCanvas.Width = image.Width;
+            FaceStackCanvas.Height = image.Height;
+
+            if (FaceStackCanvas.Children.Count > 0)
+                FaceStackCanvas.Children.Clear();
+
+            FaceStackCanvas.Children.Add(image);
+        }
+
+        private Image resizeImage(Image _image)
+        {
+            if (_image.Width < 1024 && _image.Height < 1024)
+                return _image;
+           
+            int max = _image.Width <= _image.Height ? (int)_image.Width : (int)_image.Height;
+            float ratio = (float)1024 / (float)max;
+            _image.Width = _image.Width * ratio;
+            _image.Height = _image.Height * ratio;
+
+            return _image;
+        }
+
+        private void drawFaceRectangle(string _filepath)
+        {
+            FaceRecognitionAPI api = new FaceRecognitionAPI();
+            List<List<int>> rectanglePlots =  api.GetFaceRectangleList(_filepath);
+            if (rectanglePlots.Count < 1)
+                return;
+
+            for(int i = 0; i < rectanglePlots.Count; i++)
+            {
+                Rectangle rect = new Rectangle();
+                rect.Stroke = Brushes.Red;
+                rect.StrokeThickness = 2;
+                Canvas.SetLeft(rect, rectanglePlots[i][0]);
+                Canvas.SetTop(rect, rectanglePlots[i][1]);
+                rect.Width = rectanglePlots[i][2];
+                rect.Height = rectanglePlots[i][3];
+
+                FaceStackCanvas.Children.Add(rect);
+            }
+        }
+
+        private bool checkImageExtension(string _ext)
+        {
+            if(_ext == ".jpg" ||
+                _ext == ".jpeg"||
+                _ext == ".png" ||
+                _ext == ".gif" ||
+                _ext == ".tiff" ||
+                _ext == ".tif" |
+                _ext == "bmp")
+                return true;
+
+            return false;
+        }
+    }
+}
