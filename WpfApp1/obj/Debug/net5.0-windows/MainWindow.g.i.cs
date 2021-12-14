f7d76a97195cda911d2f27e36622774148c4ef8b
dodge <20012011ryu@gmail.com>
2021-12-14 15:00:36 +0900

    python settings

--- WpfApp1/obj/Debug/net5.0-windows/MainWindow.g.i.cs
+++ WpfApp1/obj/Debug/net5.0-windows/MainWindow.g.i.cs
@@ -69,7 +69,7 @@ namespace WpfFaceID {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WpfFaceID;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WpfFaceID;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
